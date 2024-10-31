b66ffadcd96557718934937d43e2067a9cfd6c6e
Serhat <kiyarserhat@gmail.com>
2024-10-31 17:37:16 +0300

    Odev3 eklendi.

---
+++ Laboratuvar Ödevleri/Odev3/LifeGame - BagliListe/src/BagliListe/Eleman.java
@@ -0,0 +1,19 @@
+package BagliListe;
+
+public class Eleman {
+    Eleman sonraki = null;
+    String ad;
+    String soyad;
+    int telno;
+    int dt;
+
+
+    public Eleman(String ad, String soyad, int telno, int dt) {
+        this.ad = ad;
+        this.soyad = soyad;
+        this.telno = telno;
+        this.dt = dt;
+
+
+    }
+}
